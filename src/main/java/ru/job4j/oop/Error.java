19f4378f1bd4152be6f3e7952f14b8c87593c9bc
NikolayPolegaev <N5445@yandex.ru>
2021-04-04 00:06:49 +0300

    1.7. Конструктор [#505049]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,27 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+    public Error() {
+    }
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+    public void printInfo() {
+        System.out.println("Активность: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        Error error2 = new Error(true, 10, "OK");
+        Error error3 = new Error(false, 20, "False");
+        error1.printInfo();
+        error2.printInfo();
+        error3.printInfo();
+    }
+}
