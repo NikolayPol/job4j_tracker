34b159d5c3cd420b7319ade35aab92234d75b5ba
NikolayPolegaev <N5445@yandex.ru>
2021-04-04 00:45:23 +0300

    3. Перегрузить метод max для трех чисел [#505041]

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int first, int second) {
+        return Math.max(first, second);
+    }
+    public static int max(int first, int second, int third) {
+        return Max.max(Max.max(first, second), third);
+    }
+    public static int max(int first, int second, int third, int fourth) {
+        return Max.max(Max.max(first, second), Max.max(third, fourth));
+    }
+}
---
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.condition;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+
+public class MaxTest{
+    @Test
+    public void when10and20then20() {
+        int first = 10;
+        int second = 20;
+        int result = Max.max(first, second);
+        Assert.assertEquals(result, 20);
+    }
+    @Test
+    public void when10and20and30then30() {
+        int first = 10;
+        int second = 20;
+        int third = 30;
+        int result = Max.max(first, second, third);
+        Assert.assertEquals(result, 30);
+    }
+    @Test
+    public void when10and20and30and40then40() {
+        int first = 10;
+        int second = 20;
+        int third = 30;
+        int fourth = 40;
+        int result = Max.max(first, second, third, fourth);
+        Assert.assertEquals(result, 40);
+    }
+}
