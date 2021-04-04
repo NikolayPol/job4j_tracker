d87d8c046d98153e7e30e9d3f52ffae3e1ab5535
NikolayPolegaev <N5445@yandex.ru>
2021-04-04 23:34:46 +0300

    1.7. Конструктор [#505049]

--- src/main/java/ru/job4j/oop/Error.java
+++ src/main/java/ru/job4j/oop/Error.java
@@ -1,21 +1,25 @@
 package ru.job4j.oop;
 
 public class Error {
-    boolean active;
-    int status;
-    String message;
+    private boolean active;
+    private int status;
+    private String message;
+
     public Error() {
     }
+
     public Error(boolean active, int status, String message) {
         this.active = active;
         this.status = status;
         this.message = message;
     }
+
     public void printInfo() {
         System.out.println("Активность: " + active);
         System.out.println("Статус: " + status);
         System.out.println("Сообщение: " + message);
     }
+
     public static void main(String[] args) {
         Error error1 = new Error();
         Error error2 = new Error(true, 10, "OK");
