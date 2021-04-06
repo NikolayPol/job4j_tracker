bce8182672607452cf2ea7a480089363b4d492af
NikolayPolegaev <N5445@yandex.ru>
2021-04-06 20:29:25 +0300

    9. toString [#505069]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -14,5 +14,6 @@ public class StartUI {
         Tracker tracker = new Tracker();
         tracker.add(item);
         System.out.println(tracker.findById(1).getName());
+        System.out.println(tracker.findById(1).toString());
     }
 }
