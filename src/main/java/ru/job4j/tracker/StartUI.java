c6e50a271dc0c0ef2fd391d11347b9d24ecf3cc5
NikolayPolegaev <N5445@yandex.ru>
2021-05-12 17:52:57 +0300

    2.2. Реализация класса StartUI. Вывод меню [#500743 #76045]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,69 +3,16 @@ package ru.job4j.tracker;
 import java.util.Scanner;
 
 public class StartUI {
+
     public void init(Scanner scanner, Tracker tracker) {
         boolean run = true;
         while (run) {
             showMenu();
             System.out.print("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
-            if (select == 0) {
-                System.out.println("=== Create a new Item ====");
-                System.out.print("Enter name: ");
-                String name = scanner.nextLine();
-                Item item = new Item(name);
-                tracker.add(item);
-            } else if (select == 1) {
-                System.out.println("=== All items ====");
-                Item[] arr = tracker.findAll();
-                for (Item i : arr) {
-                    System.out.println(i);
-                }
-            } else if (select == 2) {
-                System.out.println("=== Edit item ====");
-                System.out.print("Enter id: ");
-                int id = Integer.parseInt(scanner.nextLine());
-                System.out.print("Enter name: ");
-                String newName = scanner.nextLine();
-                Item newItem = new Item(newName);
-                if (tracker.replace(id, newItem)) {
-                    System.out.println("Replace is successfull");
-                } else {
-                    System.out.println("Replacement caused an error");
-                }
-            } else if (select == 3) {
-                System.out.println("=== Delete item ====");
-                System.out.print("Enter id: ");
-                int id = Integer.parseInt(scanner.nextLine());
-                if (tracker.delete(id)) {
-                    System.out.println("Delete is successfull");
-                } else {
-                    System.out.println("Delete caused an error");
-                }
-            } else if (select == 4) {
-                System.out.println("=== Find item by Id ====");
-                System.out.print("Enter id: ");
-                int id = Integer.parseInt(scanner.nextLine());
-                Item rsl = tracker.findById(id);
-                if (rsl != null) {
-                    System.out.println(rsl);
-                } else {
-                    System.out.println("Item not found");
-                }
-            } else if (select == 5) {
-                System.out.println("=== Find items by name ====");
-                System.out.print("Enter name: ");
-                String name = scanner.nextLine();
-                Item[] newItem = tracker.findByName(name);
-                if (newItem.length != 0) {
-                    for (Item i : newItem) {
-                        System.out.println(i);
-                    }
-                } else {
-                    System.out.println("Item not found");
-                }
-            } else if (select == 6) {
-                System.out.println("=== Exit Program ====");
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
                 run = false;
             }
         }
