ef434e3dc0b6a8d1eeee57522c5c518db235848b
NikolayPolegaev <N5445@yandex.ru>
2021-05-15 01:37:24 +0300

    1.1. Тесты на StartUI. [#505016]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -128,7 +128,7 @@ public class StartUITest {
     public void whenInvalidExit() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"10", "0"/* Пункты меню: неверный, верный. */}
+                new String[] {"10", "0"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = new UserAction[]{
