bc9ff1d2181957c799eee743aaa211bb62b0ee15
NikolayPolegaev <N5445@yandex.ru>
2021-05-20 02:33:31 +0300

    2. Комбинированный компаратор [#10036 #71318]

--- src/main/java/ru/job4j/collection/JobByPriority.java
+++ src/main/java/ru/job4j/collection/JobByPriority.java
@@ -6,12 +6,5 @@ public class JobByPriority implements Comparator<Job> {
     @Override
     public int compare(Job o1, Job o2) {
         return Integer.compare(o1.getPriority(), o2.getPriority());
-//        if (o1.getPriority() > o2.getPriority()) {
-//            return 1;
-//        }
-//        if (o1.getPriority() < o2.getPriority()) {
-//            return -1;
-//        }
-//        return 0;
     }
 }
