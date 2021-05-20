e82129e243c7f7773e0b6d78b34551d7c8e91516
NikolayPolegaev <N5445@yandex.ru>
2021-05-21 02:03:17 +0300

    2. Список адресов [#110062]

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+    @Test
+    public void whenCollectProfileAdress() {
+        List<Profile> clients = List.of(
+                new Profile(new Address("London", "45", 10, 20)),
+                new Profile(new Address("Oslo", "56", 5, 50)),
+                new Profile(new Address("London", "67", 7, 100))
+        );
+        List<Address> explicit = List.of(
+                new Address("London", "45", 10, 20),
+                new Address("Oslo", "56", 5, 50),
+                new Address("London", "67", 7, 100)
+        );
+        Profiles profiles = new Profiles();
+        List<Address> actual = profiles.collect(clients);
+        assertEquals(actual, explicit);
+    }
+
+
+}
