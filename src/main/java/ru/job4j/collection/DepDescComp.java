e22401506b68d2185855e8730234329296963efb
NikolayPolegaev <N5445@yandex.ru>
2021-05-20 08:34:13 +0300

    2. Отсортировать департаменты [#34352 #158099]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,10 +5,9 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int rsl;
         String[] o1Words = o1.split("/");
         String[] o2Words = o2.split("/");
-        rsl = o2Words[0].compareTo(o1Words[0]);
+        int rsl = o2Words[0].compareTo(o1Words[0]);
         return rsl == 0 ? o1.compareTo(o2) : rsl;
     }
 }
