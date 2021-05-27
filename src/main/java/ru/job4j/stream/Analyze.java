7e0258b3e4d117662e9c284a415f9c5a3c8ce1bb
NikolayPolegaev <N5445@yandex.ru>
2021-05-27 05:38:47 +0300

    2. Аттестация [#49777 #160795]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -1,6 +1,9 @@
 package ru.job4j.stream;
 
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -26,14 +29,37 @@ public class Analyze {
     }
 
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
-        return List.of();
+        return stream
+                .flatMap(pupil -> pupil.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName,
+                                               LinkedHashMap::new,
+                                               Collectors.averagingDouble(Subject::getScore)))
+                .entrySet().stream()
+                .map(el -> new Tuple(el.getKey(), el.getValue()))
+                .collect(Collectors.toList());
+
     }
 
     public static Tuple bestStudent(Stream<Pupil> stream) {
-        return null;
+        return stream
+                .map(pupil -> new Tuple(pupil.getName(),
+                                        Stream.of(pupil)
+                                                .flatMap(p -> p.getSubjects().stream())
+                                                .mapToInt(Subject::getScore)
+                                                .sum()))
+                                                .max(Comparator.comparing(Tuple::getScore))
+                .orElse(new Tuple("student", 0));
     }
 
     public static Tuple bestSubject(Stream<Pupil> stream) {
-        return null;
+        return stream
+                .flatMap(pupil -> pupil.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName,
+                        //HashMap::new,
+                        Collectors.summingDouble(Subject::getScore)))
+                .entrySet().stream()
+                .map(el -> new Tuple(el.getKey(), el.getValue()))
+                .max(Comparator.comparing(Tuple::getScore))
+                .orElse(new Tuple("student", 0));
     }
 }
