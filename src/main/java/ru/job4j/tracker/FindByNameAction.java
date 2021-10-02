e49934b7cfb7ba616061c61895b18676f533360c
NikolayPolegaev <N5445@yandex.ru>
2021-10-02 22:31:53 +0300

    0. Что такое Mock-объект. [#6870 #206614]

--- src/main/java/ru/job4j/tracker/FindByNameAction.java
+++ src/main/java/ru/job4j/tracker/FindByNameAction.java
@@ -3,7 +3,6 @@ package ru.job4j.tracker;
 import java.util.List;
 
 public class FindByNameAction implements UserAction {
-
     private final Output out;
 
     public FindByNameAction(Output out) {
