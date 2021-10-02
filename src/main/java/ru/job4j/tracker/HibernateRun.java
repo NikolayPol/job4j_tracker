9f21b6c0342c72095b2a9c6bba9f495a0e089060
NikolayPolegaev <N5445@yandex.ru>
2021-10-02 22:40:29 +0300

    0. Конфигурация Hibernate [#6872 #206627]

--- src/main/java/ru/job4j/tracker/HibernateRun.java
+++ src/main/java/ru/job4j/tracker/HibernateRun.java
@@ -17,6 +17,7 @@ import java.util.List;
  */
 public class HibernateRun {
     public static void main(String[] args) {
+
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure().build();
         try {
